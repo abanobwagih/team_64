b1d76ab2c224bc6d0d2d66b624a16ee1ee125051
abdelrhmanUsama <85439625+abdelrhmanUsama@users.noreply.github.com>
2021-06-06 20:00:02 +0200

    Update distance_code.c
    initializing the variables

--- distance_code.c
+++ distance_code.c
@@ -1,1 +1,11 @@
 
+#include"conio.h"
+#include"stdio.h" 
+#include"math.h" 
+
+     void main() {    
+double theta ; // central angle
+double distance; // the distance between the two coordinates
+double Xi,Xf,Yi,Yf,X,Y,M,O;//initial and final coordinates of the target 
+double displacement;// shortest distance between the initial and final positions double r=6371;
+double r=6378;
