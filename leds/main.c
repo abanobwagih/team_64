586a9389953830a384bd957f6bf40b261aad9778
aba147 <85410121+aba147@users.noreply.github.com>
2021-06-06 15:27:57 +0200

    Update main.c

--- leds/main.c
+++ leds/main.c
@@ -6,3 +6,17 @@
 #define GREEN 0x08
 void SystemInit(){}
 	void init (void)
+		{
+		uint32_t delay;
+		SYSCTL_RCGCGPIO_R |=0x20;
+		delay =1; // dummy var 
+		GPIO_PORTF_LOCK_R=0x4C4F434B;
+		GPIO_PORTF_CR_R=0x1F;
+		GPIO_PORTF_AFSEL_R =0;
+		GPIO_PORTF_PCTL_R=0;
+		GPIO_PORTF_AMSEL_R=0;
+		GPIO_PORTF_DIR_R=0x0E;
+		GPIO_PORTF_DEN_R=0x1F;
+		GPIO_PORTF_PUR_R=0x11;
+	
+	}
