e32089c8c7514ec02d7db812af589b78db62ca01
Abanob Wagih 1802411 <bebo.w.b@gmail.com>
2021-06-06 14:37:00 +0200

    abanob wagih 1802411 edited this file

---
+++ leds/main.c
@@ -0,0 +1,8 @@
+#include"stdint.h"
+#include <TM4C123.h>
+#include"C:/Keil_v5/EE319Kware/inc/tm4c123gh6pm.h"
+#define RED 0x02
+#define BLUE 0x04
+#define GREEN 0x08
+void SystemInit(){}
+	void init (void)
