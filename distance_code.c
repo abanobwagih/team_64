a557225156303c0a1e8d36516310ab2882984bbe
aba147 <85410121+aba147@users.noreply.github.com>
2021-06-06 20:04:17 +0200

    Update distance_code.c
    the mathmatical formula and printing the approximating distance

--- distance_code.c
+++ distance_code.c
@@ -9,3 +9,16 @@ double distance; // the distance between the two coordinates
 double Xi,Xf,Yi,Yf,X,Y,M,O;//initial and final coordinates of the target 
 double displacement;// shortest distance between the initial and final positions double r=6371;
 double r=6378;
+          // the 2 upcoming lines is to get the distance between latitudes and  longitudes 
+ 
+    M=(110*(Xf-Xi)); 
+	O=(110*(Yf-Yi));
+
+X = M*M;
+Y = O*O;
+displacement = X+Y;
+theta=acos((2*r*r - displacement)/(2*r*r)); // cos rule
+distance=(theta*r);
+       printf("%lf",distance);
+
+     }
