939156ea16199fb25ea5d5c9ce08bbc41b07eaca
karim mishriqy <85436601+karimsalah917@users.noreply.github.com>
2021-06-13 23:20:14 +0200

    Update GPS Tracking System.c
    to print string (pointer to elements of array of char) on lcd

--- GPS Tracking System.c
+++ GPS Tracking System.c
@@ -70,3 +70,52 @@ void LCD_char(unsigned char data)
     GPIO_PORTA_DATA_R = 0;           //Rs=0 Rw=0 E=0(back to the beginning)
     delay_us(50);
 }
+
+
+void LCD_string(char* str) // to print string (pointer to elements of array of char)
+{
+    int i;
+    for (i = 0; str[i] != 0; i++)  /* Send each char of string  */
+    {
+        LCD_char(str[i]);  /* Call LCD data write */
+        delay_ms(20);
+    }
+}
+
+void LCD_integers(int data)
+{
+    int p;
+    int k = 0;
+    while (data > 0)
+    {
+        num[k] = data % 10;
+        data = data / 10;
+        k++;
+    }
+    k--;
+    for (p = k; p >= 0; p--)
+    {
+        c = num[p] + 48;
+
+        GPIO_PORTA_DATA_R = 0x80;       //Rs=1 send data, Rw=0 write,E=0 initially enable=0
+        delay_ms(1);
+        GPIO_PORTA_DATA_R |= 0x40;      // E=1 (low to high) without affecting other pins
+        delay_ms(1);
+        GPIO_PORTB_DATA_R = c;           // send data to the screen
+        delay_us(1);
+        GPIO_PORTA_DATA_R = 0;           //Rs=0 Rw=0 E=0(back to the beginning)
+        delay_us(50);
+    }
+}
+
+
+void LCD_separating_double(double deci)
+{
+    int integer_part = deci;
+    int decimal_part = (deci - integer_part) * 100000;
+
+    LCD_integers(integer_part);
+    LCD_char('.');
+    LCD_integers(decimal_part);
+}
+
